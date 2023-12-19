2cf79fc6516d99a3945a6fcb12f788c90f555e49
dauxuanhoanghung <dxhh2510@gmail.com>
2023-12-19 14:14:20 +0700

    add tw

--- src/contexts/index.jsx
+++ src/contexts/index.jsx
@@ -1,14 +1,17 @@
+import { DarkModeProvider } from "./DarkModeContext";
 import { OpenChatDrawerProvider } from "./OpenChatDrawerContext";
 import { SnackbarProvider } from "./SnackbarContext";
 
 export function AppProvider({ children }) {
-  return (<>
-    <SnackbarProvider>
-      <OpenChatDrawerProvider>
-        {children}
-      </OpenChatDrawerProvider>
-    </SnackbarProvider>
-  </>);
+  return (
+    <>
+      <DarkModeProvider>
+        <SnackbarProvider>
+          <OpenChatDrawerProvider>{children}</OpenChatDrawerProvider>
+        </SnackbarProvider>
+      </DarkModeProvider>
+    </>
+  );
 }
 
 export default AppProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import { store } from "./app/store";
-import 'react-toastify/dist/ReactToastify.css';
 import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
+
+import { store } from "./app/store";
+import App from "./App.jsx";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
