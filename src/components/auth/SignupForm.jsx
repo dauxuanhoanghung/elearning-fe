d06178530b78f7f99cc591c0fcbf27b3a224a9c3
dauxuanhoanghung <dxhh2510@gmail.com>
2024-01-16 11:46:54 +0700

    test vite deploy

--- src/components/auth/SignupForm.jsx
+++ src/components/auth/SignupForm.jsx
@@ -66,128 +66,128 @@ const SignupForm = () => {
     }
   };
 
-  return (
-    <>
-      <Container maxWidth="sm" sx={{ marginY: "10px" }}>
-        <Paper
-          elevation={3}
-          style={{ padding: "20px" }}
-          className=".signUp__section"
-        >
-          <Typography variant="h4" align="center" className="">
-            Register
-          </Typography>
-          <form onSubmit={handleSubmit}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6} className=".signUp__inputs">
-                <TextField
-                  label="First Name"
-                  name="firstName"
-                  variant="outlined"
-                  fullWidth
-                  required
-                  value={formData.firstName}
-                  onChange={handleChange}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  label="Last Name"
-                  name="lastName"
-                  variant="outlined"
-                  fullWidth
-                  required
-                  value={formData.lastName}
-                  onChange={handleChange}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  label="Email"
-                  name="email"
-                  type="email"
-                  variant="outlined"
-                  fullWidth
-                  required
-                  value={formData.email}
-                  onChange={handleChange}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  label="Username"
-                  name="username"
-                  variant="outlined"
-                  fullWidth
-                  required
-                  value={formData.username}
-                  onChange={handleChange}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  label="Password"
-                  name="password"
-                  type="password"
-                  variant="outlined"
-                  fullWidth
-                  required
-                  value={formData.password}
-                  onChange={handleChange}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  label="Confirm Password"
-                  name="confirmPassword"
-                  type="password"
-                  variant="outlined"
-                  fullWidth
-                  required
-                  value={formData.confirmPassword}
-                  onChange={handleChange}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <input
-                  type="file"
-                  accept=".png, .jpg, .jpeg, .bit" // Define the allowed file types
-                  style={{ display: "none" }} // Hide the default file input
-                  id="fileInput"
-                  onChange={handleFileChange}
-                />
-                <label htmlFor="fileInput">
-                  <Input
-                    fullWidth
-                    variant="outlined"
-                    readOnly
-                    value={selectedFile ? selectedFile.name : ""}
-                    endAdornment={
-                      <Button component="span">Upload Avatar</Button>
-                    }
-                  />
-                </label>
-              </Grid>
-            </Grid>
-            <div className="signUp__section" style={{ marginBottom: "5px" }}>
-              <p>Already have an account?</p>
-              <Link to="/login" style={{ color: "#a435f0" }}>
-                Log In
-              </Link>
-            </div>
-            <Button
-              type="submit"
-              variant="contained"
-              sx={{ backgroundColor: "#a435f0" }}
-              fullWidth
-            >
-              Register
-            </Button>
-          </form>
-        </Paper>
-      </Container>
-    </>
-  );
+  return <></>;
 };
 
 export default SignupForm;
+
+const Old = () => {
+  return (
+    <Container maxWidth="sm" sx={{ marginY: "10px" }}>
+      <Paper
+        elevation={3}
+        style={{ padding: "20px" }}
+        className=".signUp__section"
+      >
+        <Typography variant="h4" align="center" className="">
+          Register
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6} className=".signUp__inputs">
+              <TextField
+                label="First Name"
+                name="firstName"
+                variant="outlined"
+                fullWidth
+                required
+                value={formData.firstName}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                label="Last Name"
+                name="lastName"
+                variant="outlined"
+                fullWidth
+                required
+                value={formData.lastName}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Email"
+                name="email"
+                type="email"
+                variant="outlined"
+                fullWidth
+                required
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Username"
+                name="username"
+                variant="outlined"
+                fullWidth
+                required
+                value={formData.username}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Password"
+                name="password"
+                type="password"
+                variant="outlined"
+                fullWidth
+                required
+                value={formData.password}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Confirm Password"
+                name="confirmPassword"
+                type="password"
+                variant="outlined"
+                fullWidth
+                required
+                value={formData.confirmPassword}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <input
+                type="file"
+                accept=".png, .jpg, .jpeg, .bit" // Define the allowed file types
+                style={{ display: "none" }} // Hide the default file input
+                id="fileInput"
+                onChange={handleFileChange}
+              />
+              <label htmlFor="fileInput">
+                <Input
+                  fullWidth
+                  variant="outlined"
+                  readOnly
+                  value={selectedFile ? selectedFile.name : ""}
+                  endAdornment={<Button component="span">Upload Avatar</Button>}
+                />
+              </label>
+            </Grid>
+          </Grid>
+          <div className="signUp__section" style={{ marginBottom: "5px" }}>
+            <p>Already have an account?</p>
+            <Link to="/login" style={{ color: "#a435f0" }}>
+              Log In
+            </Link>
+          </div>
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{ backgroundColor: "#a435f0" }}
+            fullWidth
+          >
+            Register
+          </Button>
+        </form>
+      </Paper>
+    </Container>
+  );
+};
