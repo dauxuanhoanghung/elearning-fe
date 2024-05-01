1a09d0c670146c307b35442980c0176f2345cf0b
dauxuanhoanghung <dxhh2510@gmail.com>
2024-05-01 22:52:13 +0700

    remove unuse files, lecture form

--- src/pages/admin/course/update/LectureForm.jsx
+++ src/pages/admin/course/update/LectureForm.jsx
@@ -1,62 +1,71 @@
-import { useState } from "react";
+import { Trash } from "lucide-react";
+import React, { useRef, useState } from "react";
+
+import SlowMotionVideoIcon from "@mui/icons-material/SlowMotionVideo";
 import {
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
+  Card,
+  CardContent,
   FormControl,
   FormControlLabel,
   FormLabel,
   Grid,
-  InputLabel,
+  IconButton,
   Radio,
   RadioGroup,
-  TextField,
 } from "@mui/material";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { useSnackbar } from "@/contexts/SnackbarContext";
-import { lectureService } from "@/services";
 
-const LectureForm = ({
-  selectedSection,
-  setOpenLectureForm,
-  fetchSectionsAndItsLectures,
-}) => {
+const defaultLecture = {
+  title: "",
+  content: "",
+  type: "VIDEO",
+  orderIndex: "",
+  description: "",
+  duration: 0,
+  uploaderType: "YOUTUBE",
+  ["videoFile"]: null,
+};
+
+const SectionForm = ({ section, courseData, setCourseData }) => {
   const { showSnackbar } = useSnackbar();
-  // #region new lecture form
-  const [lectures, setLectures] = useState(selectedSection?.lectures || []);
-  const [lectureFormData, setLectureFormData] = useState({
-    title: "",
-    content: "",
-    type: "VIDEO",
-    orderIndex: "",
-    uploaderType: "YOUTUBE",
-    videoFile: null,
-  });
-
+  // #region lectureData
+  const [lectures, setLectures] = useState(section?.lectures || []);
+  const videoInputRef = useRef();
+  // new lecture
+  const [lectureFormData, setLectureFormData] = useState(defaultLecture);
   const handleLectureChange = (e) => {
     const { name, value, type, files } = e.target;
-    const maxSize = 100 * 1024 * 1024;
     if (type === "file") {
-      if (files[0].size > maxSize) {
-        showSnackbar({
-          message: "Video upload maximum size exceeded 1MB",
-          severity: "error",
-        });
-        return;
-      }
       setLectureFormData({ ...lectureFormData, [name]: files[0] });
     } else {
       setLectureFormData({ ...lectureFormData, [name]: value });
-      if (name === "TEXT")
+      if (name === "TEXT") {
         setLectureFormData({ ...lectureFormData, ["videoFile"]: null });
+        videoInputRef.current.value = null;
+      }
     }
   };
+
+  const createLectureForm = (lecture, sectionId) => {
+    const formData = new FormData();
+    formData.append("title", lecture.title);
+    formData.append("content", lecture.content);
+    formData.append("type", lecture.type);
+    formData.append("uploaderType", lecture.uploaderType);
+    formData.append("orderIndex", lecture.orderIndex);
+    formData.append("videoFile", lecture.videoFile);
+    formData.append("section", sectionId);
+    return formData;
+  };
+
   const addLecture = () => {
     if (
       !lectureFormData.title.trim() ||
-      (lectureFormData.type === "VIDEO" && !lectureFormData.videoFile)
+      (lectureFormData.type === "VIDEO" && !videoInputRef?.current?.files[0])
     ) {
       showSnackbar({
         message: "Please choose the file and fill the data",
@@ -64,78 +73,111 @@ const LectureForm = ({
       });
       return;
     }
-    console.log("selectedSection", selectedSection);
     const newLecture = {
       title: lectureFormData.title,
       content: lectureFormData.content,
       type: lectureFormData.type,
-      orderIndex: lectures.length + 1,
-      videoFile: lectureFormData.videoFile,
       uploaderType: lectureFormData.uploaderType,
+      description: lectureFormData.description,
+      orderIndex: lectures.length + 1,
+      duration: lectureFormData.duration,
     };
-    setLectureFormData({
-      title: "",
-      content: "",
-      type: "VIDEO",
-      uploaderType: "YOUTUBE",
-      orderIndex: "",
-      videoFile: null,
+    if (videoInputRef.current?.files[0]) {
+      newLecture.videoFile = videoInputRef.current.files[0];
+    }
+
+    const updatedLectures = [...lectures, newLecture];
+    setLectures(updatedLectures);
+    const updatedSection = { ...section, lectures: updatedLectures };
+    const updatedSections = courseData.sections.map((sec) => {
+      return sec.orderIndex === section.orderIndex ? updatedSection : sec;
     });
-    handleAddLecture(newLecture);
+
+    setCourseData({
+      ...courseData,
+      sections: updatedSections,
+    });
+
+    setLectureFormData(defaultLecture);
+    if (videoInputRef.current) videoInputRef.current.value = null;
   };
-  const handleAddLecture = async (lecture) => {
-    const createForm = (lecture, sectionId) => {
-      const formData = new FormData();
-      formData.append("title", lecture.title);
-      formData.append("content", lecture.content);
-      formData.append("type", lecture.type);
-      formData.append("orderIndex", lecture.orderIndex);
-      formData.append("videoFile", lecture.videoFile);
-      formData.append("uploaderType", lecture.uploaderType);
-      formData.append("section", sectionId);
-      return formData;
+
+  const deleteLecture = (index) => {
+    const updatedLectures = lectures.filter((_, i) => i !== index);
+    const lecturesWithUpdatedIndexes = updatedLectures.map(
+      (lecture, index) => ({ ...lecture, orderIndex: index + 1 }),
+    );
+    setLectures(lecturesWithUpdatedIndexes);
+    // Update the section's lectures in courseData
+    const updatedSection = {
+      ...section,
+      lectures: lecturesWithUpdatedIndexes,
     };
-    const lectureRequest = createForm(lecture, selectedSection.id);
-    const res = await lectureService.create(lectureRequest);
-    fetchSectionsAndItsLectures();
-    showSnackbar({ message: "Upload successfully", severity: "success" });
+
+    const updatedSections = courseData.sections.map((sec) => {
+      return sec.id === section.id ? updatedSection : sec;
+    });
+
+    setCourseData({ ...courseData, sections: updatedSections });
   };
   // #endregion
-  // #region cancel
-  const [openDialog, setOpenDialog] = useState(false);
-  const handleCancel = () => {
-    setOpenDialog(false);
-    setOpenLectureForm(false);
-  };
-  const handleCloseDialog = () => {
-    setOpenDialog(false);
-  };
-  // endregion
   return (
     <>
-      <Grid container spacing={1}>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            label="Lecture Title"
+      {lectures.map((lecture, index) => (
+        <React.Fragment key={index}>
+          <Card>
+            <CardContent sx={{ display: "flex" }}>
+              <div>
+                <span className="font-bold">
+                  {`${lecture.orderIndex}. ${lecture.title}`}
+                </span>
+                <SlowMotionVideoIcon />
+              </div>
+              <IconButton onClick={() => deleteLecture(index)}>
+                <Trash />
+              </IconButton>
+              {/* Add more content or props as needed */}
+            </CardContent>
+          </Card>
+        </React.Fragment>
+      ))}
+      {/* Render your lecture form inputs here */}
+      <div className="flex flex-col gap-2">
+        <div>
+          <Label>Lecture Title</Label>
+          <Input
+            placeholder="Enter lecture title ..."
+            className="w-full"
             name="title"
             value={lectureFormData.title}
             onChange={handleLectureChange}
           />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            placeholder="Lecture Content"
+        </div>
+        <div>
+          <Label>Lecture Content</Label>
+          <textarea
+            rows={5}
+            placeholder="Enter lecture content ..."
+            className="w-full rounded-lg border bg-gray-50 px-3.5 py-2.5 text-sm text-gray-900
+              outline-none placeholder:text-gray-400 dark:bg-gray-700 dark:text-white "
             name="content"
-            label="Description"
             value={lectureFormData.content}
-            fullWidth
-            multiline
-            rows={4}
             onChange={handleLectureChange}
           />
-        </Grid>
-        <Grid item xs={12}>
+        </div>
+        <div>
+          <Label>Description</Label>
+          <textarea
+            rows={5}
+            placeholder="Enter Description ..."
+            className="w-full rounded-lg border bg-gray-50 px-3.5 py-2.5 text-sm text-gray-900
+              outline-none placeholder:text-gray-400 dark:bg-gray-700 dark:text-white "
+            name="description"
+            value={lectureFormData.description}
+            onChange={handleLectureChange}
+          />
+        </div>
+        <div>
           <FormControl component="fieldset">
             <FormLabel component="legend">Lecture Type</FormLabel>
             <RadioGroup
@@ -152,7 +194,7 @@ const LectureForm = ({
               <FormControlLabel value="TEXT" control={<Radio />} label="Text" />
             </RadioGroup>
           </FormControl>
-        </Grid>
+        </div>
         {lectureFormData.type === "VIDEO" && (
           <>
             <Grid item xs={12}>
@@ -177,55 +219,25 @@ const LectureForm = ({
                 </RadioGroup>
               </FormControl>
             </Grid>
-            <Grid item xs={12}>
-              <InputLabel>Upload Video</InputLabel>
+            <div className="">
+              <Label>Upload Video</Label>
               <input
+                className="w-full"
                 type="file"
                 name="videoFile"
                 accept="video/*"
-                onChange={handleLectureChange}
+                ref={videoInputRef}
               />
-            </Grid>
+            </div>
           </>
         )}
-        <Grid item xs={12}>
-          <Button variant="contained" color="primary" onClick={addLecture}>
-            Add Lecture
-          </Button>
-          <Button
-            variant="contained"
-            color="secondary"
-            onClick={() => setOpenDialog(true)}
-            sx={{ marginX: "10px" }}
-          >
-            Cancel and return
-          </Button>
-        </Grid>
-
-        <Dialog open={openDialog} onClose={handleCloseDialog}>
-          <DialogTitle id="alert-dialog-title">
-            Do you want to cancel this action ?
-          </DialogTitle>
-          <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-              This action can't be redone. Are you sure to do that?
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button
-              onClick={handleCancel}
-              sx={{ backgroundColor: "#f50057", color: "#fff" }}
-            >
-              Yes
-            </Button>
-            <Button onClick={handleCloseDialog} autoFocus>
-              No
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </Grid>
+        <div className="mt-2 block">
+          <Button onClick={addLecture}>Add Lecture</Button>
+        </div>
+      </div>
+      {/* Include other lecture form inputs */}
     </>
   );
 };
 
-export default LectureForm;
+export default SectionForm;
--- src/pages/admin/course/update/index.jsx
+++ src/pages/admin/course/update/index.jsx
@@ -19,6 +19,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useSnackbar } from "@/contexts/SnackbarContext";
 import { courseService } from "@/services";
+import { stringToFormatDate } from "@/utils/date-utils";
 import { isAdmin } from "@/utils/utils";
 import LectureForm from "./LectureForm";
 
@@ -40,9 +41,6 @@ const CourseUpdatePage = () => {
     background: "",
     price: 0,
     countRegistration: 1,
-    createdDate: "",
-    publishDate: "",
-    updatedDate: "",
     user: {},
   });
 
@@ -67,11 +65,27 @@ const CourseUpdatePage = () => {
   };
   const handleUpdate = async () => {
     const formData = new FormData();
-    formData.append("name", courseData.name);
-    formData.append("price", courseData.price);
-    formData.append("description", courseData.description);
-
-    const res = await courseService.update();
+    if (inputRef.current.files[0]) {
+      formData.append("backgroundFile", inputRef.current.files[0]);
+    }
+    for (const key in courseData) {
+      console.log(key);
+      if (!data.hasOwnProperty(key)) continue;
+      if (key === "publishDate" && courseData[key] !== null) {
+        formData.append(key, stringToFormatDate(courseData[key]));
+        continue;
+      }
+      if (Array.isArray(courseData[key])) {
+        const encodedArray = courseData[key].map(encodeURIComponent);
+        formData.append(key, encodedArray.join(","));
+        continue;
+      }
+      formData.append(key, courseData[key]);
+    }
+    const res = await courseService.update(formData);
+    console.log(res);
+    if (res.status) {
+    }
   };
   // #endregion
 
@@ -111,141 +125,141 @@ const CourseUpdatePage = () => {
         <div className="col-span-4 md:col-span-1">
           <LectureList isCourseDetailPage={true} />
         </div>
-        {isLoading ? (
-          <Spinner />
-        ) : (
-          <div className="col-span-4 mb-6 md:col-span-3">
+        {isLoading && <Spinner />}
+        <div className="col-span-4 mb-6 md:col-span-3">
+          <div data-role="create-lecture-form">
             {openLectureForm && (
               <LectureForm
                 courseData={courseData}
                 setCourseData={setCourseData}
-                selectedSection={selectedSection}
                 setOpenLectureForm={setOpenLectureForm}
-                fetchSectionsAndItsLectures={fetchSectionsAndItsLectures}
               />
             )}
-            <div data-role="course-update-form">
-              {!openLectureForm && (
-                <div
-                  className="mt-4 grid grid-cols-1 gap-y-2"
-                  data-role="start-form"
-                >
-                  <div className="w-full">
-                    <label
-                      htmlFor="background-image-upload"
-                      className="flex h-64 w-full cursor-pointer flex-col items-center justify-center overflow-hidden 
+          </div>
+          <div data-role="course-update-form">
+            {!openLectureForm && (
+              <div
+                className="mt-4 grid grid-cols-1 gap-y-2"
+                data-role="start-form"
+              >
+                <div className="w-full">
+                  <label
+                    htmlFor="background-image-upload"
+                    className="flex h-64 w-full cursor-pointer flex-col items-center justify-center overflow-hidden 
                   rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 
                   dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-                    >
-                      {backgroundImageURL ? (
-                        <img
-                          src={backgroundImageURL}
-                          className="w-full object-cover"
-                        />
-                      ) : (
-                        <div className="flex flex-col items-center justify-center pb-6 pt-5 text-gray-500 dark:text-gray-400">
-                          <UploadIcon />
-                          <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                            <span className="font-semibold">
-                              {t("signup.click")}
-                            </span>
-                            {t("signup.dnd")}
-                          </p>
-                          <p className="text-xs text-gray-500 dark:text-gray-400">
-                            {t("signup.type")}
-                          </p>
-                        </div>
-                      )}
-                    </label>
-                    {/* Hidden file input */}
-                    <input
-                      type="file"
-                      accept="image/*"
-                      id="background-image-upload"
-                      ref={inputRef} // Ref for the hidden file input
-                      className="hidden"
-                      onChange={handleBackgroundImageUpload}
-                    />
-                  </div>
-                  <div className="w-full">
-                    <label
-                      htmlFor="name"
-                      className="mb-1 block text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                      Course Name
-                    </label>
-                    <Input
-                      type="text"
-                      name="name"
-                      id="name"
-                      placeholder="Enter your course name"
-                      value={courseData.name}
-                      onChange={handleInputChange}
-                      required
-                    />
-                  </div>
-                  <div className="w-full">
-                    <label
-                      htmlFor="subtitle"
-                      className="mb-1 block text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                      <span>Subtitle</span>
-                      <span className="ml-2 text-gray-700 dark:text-gray-300/80">
-                        (More detail of your course)
-                      </span>
-                    </label>
-                    <Input
-                      type="text"
-                      name="subtitle"
-                      id="subtitle"
-                      placeholder="Enter your course subtitle"
-                      value={courseData.subtitle}
-                      onChange={handleInputChange}
-                      required
-                    />
-                  </div>
-                  <div className="w-full">
-                    <label className="mb-1 block text-sm font-medium text-gray-900 dark:text-white">
-                      Price
-                    </label>
-                    <Input
-                      type="number"
-                      name="price"
-                      id="price"
-                      placeholder="Enter price"
-                      value={courseData.price}
-                      onChange={handleInputChange}
-                      required
-                    />
-                  </div>
-                  <div className="w-full">
-                    <label className="mb-1 block text-sm font-medium text-gray-900 dark:text-white">
-                      Description
-                    </label>
-                    <textarea
-                      rows={4}
-                      value={courseData.description}
-                      onChange={handleInputChange}
-                      name="description"
-                      className="w-full rounded-lg border bg-gray-50 px-3.5 py-2.5 text-sm text-gray-900
-                    outline-none placeholder:text-gray-400 dark:bg-gray-700 dark:text-white "
-                      placeholder="Example: This course will provide some information about some fields, ... It helps you improve your skills in ..."
-                      required
-                    ></textarea>
-                  </div>
-                  <div className="flex-start flex gap-4">
-                    <Button className="dark:bg-gray-900" onClick={handleUpdate}>
-                      Update
-                    </Button>
-                    <Button className="dark:bg-gray-900" onClick={handleUpdate}>
-                      Create a lecture
-                    </Button>
-                  </div>
+                  >
+                    {backgroundImageURL ? (
+                      <img
+                        src={backgroundImageURL}
+                        className="w-full object-cover"
+                      />
+                    ) : (
+                      <div className="flex flex-col items-center justify-center pb-6 pt-5 text-gray-500 dark:text-gray-400">
+                        <UploadIcon />
+                        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                          <span className="font-semibold">
+                            {t("signup.click")}
+                          </span>
+                          {t("signup.dnd")}
+                        </p>
+                        <p className="text-xs text-gray-500 dark:text-gray-400">
+                          {t("signup.type")}
+                        </p>
+                      </div>
+                    )}
+                  </label>
+                  {/* Hidden file input */}
+                  <input
+                    type="file"
+                    accept="image/*"
+                    id="background-image-upload"
+                    ref={inputRef} // Ref for the hidden file input
+                    className="hidden"
+                    onChange={handleBackgroundImageUpload}
+                  />
                 </div>
-              )}
-            </div>
+                <div className="w-full">
+                  <label
+                    htmlFor="name"
+                    className="mb-1 block text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Course Name
+                  </label>
+                  <Input
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="Enter your course name"
+                    value={courseData.name}
+                    onChange={handleInputChange}
+                    required
+                  />
+                </div>
+                <div className="w-full">
+                  <label
+                    htmlFor="subtitle"
+                    className="mb-1 block text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    <span>Subtitle</span>
+                    <span className="ml-2 text-gray-700 dark:text-gray-300/80">
+                      (More detail of your course)
+                    </span>
+                  </label>
+                  <Input
+                    type="text"
+                    name="subtitle"
+                    id="subtitle"
+                    placeholder="Enter your course subtitle"
+                    value={courseData.subtitle}
+                    onChange={handleInputChange}
+                    required
+                  />
+                </div>
+                <div className="w-full">
+                  <label className="mb-1 block text-sm font-medium text-gray-900 dark:text-white">
+                    Price
+                  </label>
+                  <Input
+                    type="number"
+                    name="price"
+                    id="price"
+                    placeholder="Enter price"
+                    value={courseData.price}
+                    onChange={handleInputChange}
+                    required
+                  />
+                </div>
+                <div className="w-full">
+                  <label className="mb-1 block text-sm font-medium text-gray-900 dark:text-white">
+                    Description
+                  </label>
+                  <textarea
+                    rows={4}
+                    value={courseData.description}
+                    onChange={handleInputChange}
+                    name="description"
+                    className="w-full rounded-lg border bg-gray-50 px-3.5 py-2.5 text-sm text-gray-900
+                    outline-none placeholder:text-gray-400 dark:bg-gray-700 dark:text-white "
+                    placeholder="Example: This course will provide some information about some fields, ... It helps you improve your skills in ..."
+                    required
+                  ></textarea>
+                </div>
+                <div className="flex-start flex gap-4">
+                  <Button className="dark:bg-gray-900" onClick={handleUpdate}>
+                    Update
+                  </Button>
+                  <Button
+                    className="dark:bg-gray-900"
+                    onClick={() => setOpenLectureForm(true)}
+                  >
+                    Create a lecture
+                  </Button>
+                </div>
+              </div>
+            )}
           </div>
-        )}
+        </div>
       </div>
     </main>
   );
