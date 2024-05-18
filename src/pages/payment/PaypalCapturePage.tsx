86edb8b8e4c70ac4224113ada520a30eeb889939
dauxuanhoanghung <dxhh2510@gmail.com>
2024-05-18 09:41:45 +0700

    checkout

--- src/pages/payment/PaypalCapturePage.tsx
+++ src/pages/payment/PaypalCapturePage.tsx
@@ -4,20 +4,27 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import useCountdownRedirect from "@/hooks/useCountdownRedirect";
 import registrationService from "@/services/registration.service";
 
-const PaypalCapturePage = () => {
+const PaypalCapturePage: React.FC = () => {
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
 
   const [redirectUrl, setRedirectUrl] = useState("/");
 
   useEffect(() => {
-    const token = searchParams.get("token");
-    if (!token) {
-      navigate("/");
-    }
-    const res = registrationService.capturePaypal({ token });
-    if (res) {
-    }
+    const handleResponse = async () => {
+      const token = searchParams.get("token");
+      if (!token) {
+        navigate("/");
+      }
+      const res = await registrationService.capturePaypal({ token });
+      console.log(res);
+      if (res) {
+        setRedirectUrl(
+          `/course/${res.data.courseId}/learning?lectureId=${res.data.lecture.id}`,
+        );
+      }
+    };
+    handleResponse();
   }, []);
 
   const countdownDuration = 5;
@@ -49,9 +56,9 @@ const PaypalCapturePage = () => {
             Payment successfully
           </h2>
           <p className="mb-6 text-center dark:text-gray-200">
-            <p className="text-lg">
+            <h1 className="text-lg">
               Your payment has been received. Thank you for your purchase!
-            </p>
+            </h1>
             <p>You'll be redirected to the course after {countdown} seconds.</p>
           </p>
         </div>
